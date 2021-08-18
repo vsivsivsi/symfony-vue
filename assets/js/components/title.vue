870158a66c2f51b3d05d1011eb0cbcff931d55c1
sergii.vovk <sergii.vovk@rgc.ua>
2021-08-18 13:10:23 +0300

    Vue components, Products page

---
+++ assets/js/components/title.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1>{{text}}</h1>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "title",
+    props: {
+      text: {
+        type: String,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
